b9499d23e786daad820476de340b5c4bc0e24906
mikkiclarke <127144192+mikkiclarke@users.noreply.github.com>
2023-03-08 21:00:03 -0500

    Update version_control.py

--- version_control.py
+++ version_control.py
@@ -17,6 +17,23 @@ def encoder(password):
         encoded = encoded+table[num]
     return encoded
 
+def decode(password):
+    password = encoder(password)
+    pass_list: list[password]
+    for i in password:
+        j = int(i)
+        if j > 2:
+            k = j - 3
+            print(k, end="")
+        if j < 3:
+            if j == 0:
+                print("7", end="")
+            if j == 1:
+                print("8", end="")
+            if j == 2:
+                print("9", end="")
+
+
 
 def main():
     while True:
@@ -26,7 +43,10 @@ def main():
         if option == 1:
             password = input("Please enter your password to encode: ")
             print("Your password has been encoded and stored!\n")
-        # add decoder part here
+        if option == 2:
+            print("The encoder function is ", end="")
+            print(encoder(password), end="")
+            print(" and the original password is ", end="")
         if option == 3:
             break
 
